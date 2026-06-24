b85f6476c509127aae82ceacb7b048fdce8fe460
Aravind M <heyaravind2006@gmail.com>
2026-06-24 11:52:30 +0530

    Backend Init with Server.js + db.js

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "Arise Backend Running"
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server Running On Port ${process.env.PORT}`);
+});
