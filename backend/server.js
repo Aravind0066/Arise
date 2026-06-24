142644c7c848fcdd5d090bae4c209e2a314f8aea
Aravind M <heyaravind2006@gmail.com>
2026-06-24 21:29:00 +0530

    Added User model, Route and Controller

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
 const connectDB = require("./config/db");
-
 const app = express();
+
+const User = require("./models/User");
 
 app.use(cors());
 app.use(express.json());
@@ -20,4 +20,19 @@ app.get("/", (req, res) => {
 
 app.listen(process.env.PORT, () => {
   console.log(`Server Running On Port ${process.env.PORT}`);
+});
+
+app.get("/test-user", async (req, res) => {
+  try {
+    const user = await User.create({
+      name: "Arav",
+      email: "arav@gmail.com",
+    });
+
+    res.json({user, message: "Yup, rest assured the User is working as intended...!!!"});
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
